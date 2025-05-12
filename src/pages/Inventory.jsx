24cd04c1833aba90216f4ba150f4bfa86298dbb5
himanshu <vinesh.dod@gmail.com>
2025-05-13 01:33:12 +0530

    add point on globe & working on inventory changes

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -162,11 +162,11 @@ const Inventory = ({ data }) => {
                 alt="Floor Plan"
                 className="max-w-full max-h-[80vh]"
               />
-              <img
+              {/* <img
                 src={isTypicalPlan?.image}
                 alt="Floor Plan"
                 className="max-w-full max-h-[80vh]"
-              />
+              /> */}
             </div>
           )}
         </CustomModal>
